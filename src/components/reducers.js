fc74a3268c797f693e706ee3a80beb59a04f6ad3
MariahAdams <mariah.adams23@yahoo.com>
2018-09-23 16:17:23 -0700

    successfully returns default state and loads colors

--- src/components/reducers.js
+++ src/components/reducers.js
@@ -1,7 +1,9 @@
+export const COLORS_LOAD = 'COLORS_LOAD';
 
-
-export function colors(state = [], { type }) {
+export function colors(state = [], { type, payload }) {
   switch(type) {
+    case COLORS_LOAD:
+      return payload;
     default: 
       return state;
   }
--- src/components/reducers.test.js
+++ src/components/reducers.test.js
@@ -1,9 +1,19 @@
-import { colors } from './reducers';
+import { colors, COLORS_LOAD } from './reducers';
 
 describe('color reducers', () => {
+
+  const color = ['green', 'red'];
 
   it('defaults to an empty array', () => {
     const state = colors(undefined, {});
     expect(state).toEqual([]);
+  });
+
+  it('loads colors', () => {
+    const state = colors([], {
+      type: COLORS_LOAD,
+      payload: color
+    });
+    expect(state).toEqual(color);
   });
 });
