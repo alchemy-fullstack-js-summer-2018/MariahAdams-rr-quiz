103f71906c6d5439955e280e9c847a7f6f4e43a7
MariahAdams <mariah.adams23@yahoo.com>
2018-09-23 16:53:03 -0700

    added selectors

--- src/components/reducers.js
+++ src/components/reducers.js
@@ -2,6 +2,9 @@ export const COLORS_LOAD = 'COLORS_LOAD';
 export const COLORS_ADD = 'COLORS_ADD';
 export const SELECT_COLOR = 'SELECT_COLOR';
 
+export const getColors = state => state.colors;
+export const getSelectedColor = state => state.selectedColor;
+
 export function colors(state = [], { type, payload }) {
   switch(type) {
     case COLORS_LOAD:
