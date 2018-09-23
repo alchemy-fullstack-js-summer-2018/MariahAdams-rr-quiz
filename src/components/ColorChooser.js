3b57a0c98a3d77a2903993862859d2d401d0ec5e
MariahAdams <mariah.adams23@yahoo.com>
2018-09-23 16:02:07 -0700

    wiring up rainbow light bulb and color chooser components

---
+++ src/components/ColorChooser.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ColorChooser extends Component {
+  
+  render() { 
+    return (
+      <h2>ColorChooser</h2>
+    );
+  }
+}
+ 
+export default ColorChooser;
---
+++ src/components/RainbowLightBulb.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class RainbowLightBulb extends Component {
+  
+  render() { 
+
+    return (
+      <section>
+        <h2>RainbowLightBulb</h2>
+      </section>
+    );
+  }
+}
+ 
+export default RainbowLightBulb;
