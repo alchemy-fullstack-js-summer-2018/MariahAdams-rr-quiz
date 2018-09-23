1b3e2fa79e8ff915cd214791c22cd5cbf773ba3f
MariahAdams <mariah.adams23@yahoo.com>
2018-09-23 16:58:01 -0700

    hooking up color chooser to the store

--- src/components/ColorChooser.js
+++ src/components/ColorChooser.js
@@ -1,18 +1,47 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Color from './Color';
 import AddColor from './AddColor';
+import { getColors, getSelectedColor } from './reducers';
+import { loadColors, addColor, selectColor } from './actions';
 
 class ColorChooser extends Component {
+
+  static propTypes = {
+    colors: PropTypes.array,
+    selectedColor: PropTypes.string,
+    loadColors: PropTypes.func
+  };
+
+  componentDidMount() {
+    this.props.loadColors(['green', 'blue', 'purple']);
+  }
   
   render() { 
+    const { selectedColor } = this.props;
+
     return (
       <Fragment>
         <h2>ColorChooser</h2>
-        <Color/>
-        <AddColor/>
+
+        <Color
+          selectedColor={selectedColor}
+        />
+
+        <AddColor
+        
+        />
+
       </Fragment>
     );
   }
 }
  
-export default ColorChooser;
+export default connect(
+  state => ({
+    colors: getColors(state),
+    selectedColor: getSelectedColor(state)
+  }),
+  { loadColors, selectColor, addColor }
+)(ColorChooser);
