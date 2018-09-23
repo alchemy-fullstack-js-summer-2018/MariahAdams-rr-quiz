64f7d8d29d48d6a9fdae3f6aadae36113973e291
MariahAdams <mariah.adams23@yahoo.com>
2018-09-23 16:04:34 -0700

    wiring up color and add color components

--- src/components/ColorChooser.js
+++ src/components/ColorChooser.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Color from './Color';
+import AddColor from './AddColor';
 
 class ColorChooser extends Component {
   
   render() { 
     return (
-      <h2>ColorChooser</h2>
+      <Fragment>
+        <h2>ColorChooser</h2>
+        <Color/>
+        <AddColor/>
+      </Fragment>
     );
   }
 }
