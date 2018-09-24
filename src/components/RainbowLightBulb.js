962bd760e3cc9ff3b966739e8986e16f745d9de2
MariahAdams <mariah.adams23@yahoo.com>
2018-09-23 17:59:00 -0700

    on button toggle added

--- src/components/RainbowLightBulb.js
+++ src/components/RainbowLightBulb.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getSelectedColor } from './reducers';
 
 class RainbowLightBulb extends Component {
+
+  state = {
+    on: true
+  };
+
+  static propTypes = {
+    selectedColor: PropTypes.string
+  };
+
+  handleClick = () => {
+    this.setState(({ on }) => ({ on: !on }));
+  };
   
   render() { 
+    
 
     return (
       <section>
         <h2>RainbowLightBulb</h2>
+
+        <button onClick={this.handleClick} style={{ backgroundColor: this.state.on ? 'green' : 'red' }}>
+          {this.state.on ? 'ON' : 'OFF'}
+        </button>
       </section>
     );
   }
 }
  
-export default RainbowLightBulb;
+export default connect(
+  state => ({
+    selectedColor: getSelectedColor(state)
+  })
+)(RainbowLightBulb);
